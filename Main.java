25d335e3e4b17bc58f100326d96f650a2e33499b
K L A T NISHADYA <as20240979@sci.sjp.ac.lk>
2026-05-04 21:23:00 +0530

    Initial working program with add and view

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    static ArrayList<String> students = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println("Student Record Manager");
+
+        addStudent("Alice");
+        addStudent("Bob");
+
+        viewStudents();
+    }
+
+    static void addStudent(String name) {
+        students.add(name);
+        System.out.println(name + " added.");
+    }
+
+    static void viewStudents() {
+        System.out.println("Student List:");
+        for (String s : students) {
+            System.out.println(s);
+        }
+    }
+}
